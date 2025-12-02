73e0f3347a96b8ab4a5295e39434b7f61e4e8034
JB Work Laptop <208363846+Honour-Code-Web@users.noreply.github.com>
2025-12-02 12:41:47 +0000

    fill pages with initial content

--- app.js
+++ app.js
@@ -3,11 +3,27 @@ const app = express();
 const path = require("node:path");
 
 const productsRouter = require("./routes/productsRouter");
+const customersRouter = require("./routes/customersRouter");
+const inventoryRouter = require("./routes/inventoryRouter");
+const ordersRouter = require("./routes/ordersRouter");
+const suppliersRouter = require("./routes/suppliersRouter");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use("/products", productsRouter);
+app.use("/customers", customersRouter);
+app.use("/inventory", inventoryRouter);
+app.use("/orders", ordersRouter);
+app.use("/suppliers", suppliersRouter);
+
+app.locals.links = [
+  { href: "/", text: "Home" },
+  { href: "/products", text: "Products" },
+  { href: "/customers", text: "Customers" },
+  { href: "/inventory", text: "Inventory" },
+  { href: "/suppliers", text: "Suppliers" },
+]
 
 app.get("/", function(req, res) {
   res.render("index");
